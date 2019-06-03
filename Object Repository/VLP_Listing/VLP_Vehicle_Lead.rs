a399c5999e35499b4b171c4b8f39944d43a15c9d
ricardo.martinez <ricardo.martinez@10.134.123.63>
2019-06-03 14:32:42 -0500

    Fixes missing profile value

---
+++ Object Repository/VLP_Listing/VLP_Vehicle_Lead.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Vehicle Lead button</description>
+   <name>VLP_Vehicle_Lead</name>
+   <tag></tag>
+   <elementGuidId>7a6fe0d8-1332-4c13-9731-962288d4c95f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(@href,&quot;/vehiclelead-form.htm&quot;)]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
